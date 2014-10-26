093d77caabb6dde645c7e546cc1a790d75508212
jonas.oeschger@students.fhnw.ch <jonas.oeschger@students.fhnw.ch>
2014-10-26 18:52:37 +0100

    Demo 2: - Fake Server mit einem Zertifikat, welchem der Client nicht vertraut - Client übermittelt keine Login Informationen

--- src/ch/fhnw/apsi/lab1/Demo1.java
+++ src/ch/fhnw/apsi/lab1/Demo1.java
@@ -5,11 +5,15 @@ import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Demo1 {
   
   private static String mail = "test@test.com";
   private static String password = "1234";
+  
+  private static final Logger LOGGER = Logger.getLogger(Demo1.class.getName());
 
   public static void main(String[] args) throws IOException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, CertificateException {
     // Demonstration that the authentication of the client of a subsequent request works with a cookie
@@ -23,11 +27,15 @@ public class Demo1 {
     client.setUpConnection();
 
     // Step 3: post login credentials, retrieve cookie and get the secret information
+    LOGGER.log(Level.INFO, "Authenticate with login credentials");
     String loginResponse = client.post("mail="+mail+"&password="+password);
+    LOGGER.log(Level.INFO, "Login response:");
     System.out.println(loginResponse);
     
     // Step 4: get the secret information using the cookie as the authenticator
+    LOGGER.log(Level.INFO, "Authenticate with retrieved cookie");
     String cookieResponse = client.get();
+    LOGGER.log(Level.INFO, "Cookie authentication response:");
     System.out.println(cookieResponse);
      
     server.stopServer();
--- src/ch/fhnw/apsi/lab1/Demo2.java
+++ src/ch/fhnw/apsi/lab1/Demo2.java
@@ -5,32 +5,44 @@ import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.net.ssl.SSLHandshakeException;
 
 public class Demo2 {
   
   private static String mail = "test@test.com";
   private static String password = "1234";
+  
+  private static final Logger LOGGER = Logger.getLogger(Demo2.class.getName());
 
   public static void main(String[] args) throws IOException, KeyManagementException, KeyStoreException, NoSuchAlgorithmException, CertificateException {
     // Demonstration that the authentication of the client of a subsequent request works with a cookie
     
     // Step 1: start server
-    SimpleSSLServer server = new SimpleSSLServer();
-    server.startServer();
+    FakeServer fakeServer = new FakeServer();
+    fakeServer.startServer();
     
     // Step 2: initialize client and SSL context
     SimpleSSLClient client = new SimpleSSLClient();
     client.setUpConnection();
 
-    // Step 3: post login credentials, retrieve cookie and get the secret information
-    String loginResponse = client.post("mail="+mail+"&password="+password);
-    System.out.println(loginResponse);
-    
-    // Step 4: get the secret information using the cookie as the authenticator
-    String cookieResponse = client.get();
-    System.out.println(cookieResponse);
+    // Step 3: SSL handshake won't succeed since the server presents a certificate that we don't trust
+    // --> donp't send login credentials!
+    try {
+      LOGGER.log(Level.SEVERE, "Authentication with login credentials to a fake server!!");
+      String loginResponse = client.post("mail="+mail+"&password="+password);
+      System.out.println(loginResponse);
+    } catch (SSLHandshakeException e) {
+      LOGGER.log(Level.INFO, "Fake server identified! Don't send login credentials!");
+      System.out.println(e.getCause());
+      return;
+    } finally {
+      fakeServer.stopServer();
+    }
      
-    server.stopServer();
+    
   }
 
 }
